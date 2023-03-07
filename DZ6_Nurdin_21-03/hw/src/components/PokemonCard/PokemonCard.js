841a84b928240b1225a6076e197627a88f4a755f
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-03-07 23:23:05 +0600

    Add files via upload

---
+++ DZ6_Nurdin_21-03/hw/src/components/PokemonCard/PokemonCard.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import polemonCardCss from "./PokemonCard.module.css";
+
+const PokemonCard = ({ card }) => {
+  const [listUrl, setListUrl] = useState([]);
+  const cardURL = card.url;
+  // console.log(cardURL, "--- cardURL");
+
+  useEffect(() => {
+    axios
+      .get(cardURL)
+      .then((res) => res.data)
+      .then((info) => {
+        setListUrl(info.sprites.other.dream_world.front_default);
+        // console.log(listUrl, "listUrl");
+      });
+  }, [listUrl, cardURL]);
+  return (
+    <div className={polemonCardCss.parent_pakemon}>
+      <ul className={polemonCardCss.ul_parent_for_name}>
+        <li>
+          <h2 className={polemonCardCss.pokemon_names}>{card.name}</h2>
+        </li>
+        <li>
+          <img
+            className={polemonCardCss.img_pokemons}
+            src={listUrl}
+            alt="photo"
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default PokemonCard;
